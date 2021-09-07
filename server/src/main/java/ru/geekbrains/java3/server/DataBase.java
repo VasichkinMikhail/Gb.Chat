183f4181920e48775c035cfcae044b48afcb9fe9
Mikhail Vasichkin <86611506+VasichkinMikhail@users.noreply.github.com>
2021-09-07 23:05:17 +0300

    1)Создал и связал базу данных с чатом 2)Добавил вход через логин и пароль 3)Добавил возможность изменения Nickname

---
+++ server/src/main/java/ru/geekbrains/java3/server/DataBase.java
@@ -0,0 +1,98 @@
+package ru.geekbrains.java3.server;
+
+import java.sql.*;
+
+public class DataBase{
+    private static Connection connection;
+    private static Statement statement;
+    private static PreparedStatement getUserNicknameStatement;
+    private static PreparedStatement changeUserNicknameStatement;
+    private static PreparedStatement createUserStatement;
+
+
+    public static boolean run() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:javadb.db");
+            System.out.println("База данных подключена");
+            statement = connection.createStatement();
+            createTable();
+            insert("Bob","321","Boby88");
+            prepareAllStatement();
+            return true;
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            return false;
+        }
+    }
+
+    private static void createTable() throws SQLException {
+        statement.executeUpdate("create table if not exists BaseUserChat(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "login  TEXT," +
+                "pass TEXT,"+
+                "nickname TEXT"+
+                ")");
+
+    }
+
+    public static void insert(String login,String pass, String nickname) throws SQLException {
+        try (final PreparedStatement ps = connection.prepareStatement
+                ("insert into BaseUserChat (login,pass,nickname) values (?,?,?)")) {
+            ps.setString(1, login);
+            ps.setString(2, pass);
+            ps.setString(3, nickname);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    public static void prepareAllStatement() throws SQLException {
+        getUserNicknameStatement = connection.prepareStatement("SELECT nickname FROM BaseUserChat WHERE login = ? AND pass = ?;");
+        changeUserNicknameStatement = connection.prepareStatement("UPDATE BaseUserChat SET nickname = ? WHERE nickname = ?;");
+
+    }
+
+
+    public static String getUserNickname(String login, String pass) {
+        String nickname = null;
+        try {
+            getUserNicknameStatement.setString(1, login);
+            getUserNicknameStatement.setString(2, pass);
+            ResultSet rs = getUserNicknameStatement.executeQuery();
+            if (rs.next()) {
+                nickname = rs.getString(1);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return nickname;
+    }
+
+    public static void disconnect() {
+        try {
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static boolean changeUserNickname(String currentNickname, String newNickname) {
+        try {
+            changeUserNicknameStatement.setString(1, newNickname);
+            changeUserNicknameStatement.setString(2, currentNickname);
+            changeUserNicknameStatement.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+
+}
